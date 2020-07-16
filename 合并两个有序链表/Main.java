691a693401697f4236efc376a3a8a475deea4463
PisecesPeng <piseces.peng@gmail.com>
2020-07-16 17:56:45 +0800

    update Main.java

--- 合并两个有序链表/Main.java
+++ 合并两个有序链表/Main.java
@@ -1,39 +1,45 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
         // 有序链表
-        List<Integer> list1 = new ArrayList<Integer>() {{
-            add(1);
-            add(2);
-            add(4);
-        }};
-        List<Integer> list2 = new ArrayList<Integer>() {{
-            add(1);
-            add(3);
-            add(4);
-        }};
+        ListNode node1 = new ListNode(1);
+        ListNode na = new ListNode(2);
+        na.next = new ListNode(4);
+        node1.next = na;
+        ListNode node2 = new ListNode(1);
+        ListNode nb = new ListNode(3);
+        nb.next = new ListNode(4);
+        node2.next = nb;
 
-        System.out.println(func(list1, list2));
+        func(node1, node2);
     }
 
-    private static List<Integer> func(List<Integer> list1, List<Integer> list2) {
-        list1.addAll(list2);  // 将链表先合并
-        int maxSize = list1.size();  // 链表大小
-        // 冒泡排序
-        for (int i = 0; i < maxSize; i++) {
-            for (int j = 0; j < maxSize - i; j++) {
-                if ((j + 1) >= maxSize) break;
-                if (list1.get(j) > (list1.get(j + 1))) {
-                    int swap = list1.get(j);
-                    list1.set(j, list1.get(j + 1));
-                    list1.set(j + 1, swap);
-                }
+    private static ListNode func(ListNode node1, ListNode node2) {
+        ListNode result = new ListNode(0);
+        ListNode tmp = result;
+        // 循环两个链表, 直到其中一个到底
+        while (node1 != null && node2 != null) {
+            // 每次选择val最小的node, 作为返回的node
+            if (node1.val > node2.val) {
+                ListNode swap = node1;
+                node1 = node2;
+                node2 = swap;
             }
+            tmp.next = node1;
+            node1 = node1.next;
+            tmp = tmp.next;
         }
-        return list1;
+        // 获得未到底的链表, 继续赋值
+        tmp.next = node1 != null ? node1 : node2 ;
+        return result.next;
     }
 
+    public static class ListNode {
+        int val;  // 当前链表值
+        ListNode next;  // 下一个链表对象
+
+        ListNode(int x) {  //赋值链表的值
+            val = x;
+        }
+    }
 }
